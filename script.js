800624496deadf60e6f393bd29a41cdaf67a7c7e
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-05-05 21:02:22 -0600

    code optimization

--- script.js
+++ script.js
@@ -2,66 +2,43 @@
 const typeSelector = document.getElementById("type");
 const algorithmSelector = document.getElementById("algorithm");
 
-// Create elements
-const fcfs = document.createElement("option");
-fcfs.value = "fcfs";
-fcfs.text = "First Come First Serve";
-const fifo = document.createElement("option");
-fifo.value = "fifo";
-fifo.text = "First In First Out";
-const rr = document.createElement("option");
-rr.value = "rr";
-rr.text = "Round Robin";
-const sjf = document.createElement("option");
-sjf.value = "sjf";
-sjf.text = "Shortest Job First";
-const srt = document.createElement("option");
-srt.value = "srt";
-srt.text = "Shortest Remaining Time";
-const hrrn = document.createElement("option");
-hrrn.value = "hrrn";
-hrrn.text = "Highest Response-Ratio Next";
-const mfq = document.createElement("option");
-mfq.value = "mfq";
-mfq.text = "Multilevel Feedback Queues";
-const lru = document.createElement("option");
-lru.value = "lru";
-lru.text = "Least Recently Used";
-const lfu = document.createElement("option");
-lfu.value = "lfu";
-lfu.text = "Least Frequently Used";
-const nru = document.createElement("option");
-nru.value = "nru";
-nru.text = "Not Recently Used";
-const rpo = document.createElement("option");
-rpo.value = "rpo";
-rpo.text = "Reemplazo de Página Optimal";
-
+const schedulingAlgorithms = [
+    { value: "fcfs", text: "First Come First Serve" },
+    { value: "fifo", text: "First In First Out" },
+    { value: "rr", text: "Round Robin" },
+    { value: "sjf", text: "Shortest Job First" },
+    { value: "srt", text: "Shortest Remaining Time" },
+    { value: "hrrn", text: "Highest Response-Ratio Next" },
+    { value: "mfq", text: "Multilevel Feedback Queues" },
+];
+  
+const pageReplacementAlgorithms = [
+    { value: "fcfs", text: "First Come First Serve" },
+    { value: "lru", text: "Least Recently Used" },
+    { value: "lfu", text: "Least Frequently Used" },
+    { value: "nru", text: "Not Recently Used" },
+    { value: "rpo", text: "Reemplazo de Página Optimal" },
+];
+  
 // Show different algorithms depending on the selector type 
 const setAlgorithms = () => {
     // Remove all options
-    Array.from(algorithmSelector.options).forEach(option => {
-        algorithmSelector.remove(option.index);
-    });
-
+    algorithmSelector.innerHTML = "";
+  
     // Add new options
-    if (typeSelector.value == "scheduling") {
-        algorithmSelector.add(fcfs);
-        algorithmSelector.add(fifo);
-        algorithmSelector.add(rr);
-        algorithmSelector.add(sjf);
-        algorithmSelector.add(srt);
-        algorithmSelector.add(hrrn);
-        algorithmSelector.add(mfq);
-    } else {
-        algorithmSelector.add(fcfs);
-        algorithmSelector.add(lru);
-        algorithmSelector.add(lfu);
-        algorithmSelector.add(nru);
-        algorithmSelector.add(rpo);
-    }
-    fcfs.selected = true;
-}
+    const algorithms = typeSelector.value === "scheduling"
+        ? schedulingAlgorithms
+        : pageReplacementAlgorithms;
+  
+    algorithms.forEach(algorithm => {
+      const option = document.createElement("option");
+      option.value = algorithm.value;
+      option.text = algorithm.text;
+      algorithmSelector.add(option);
+    });
+  
+    algorithmSelector.value = "fcfs";
+};
 
 function Process(id, arrival, burst, status) {
     this.id = id;
