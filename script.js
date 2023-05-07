2b6931cb59b5b4e3b011f3cc0a10283e6dd5c621
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-05-06 23:29:28 -0600

    execute proper functions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const typeSelector = document.getElementById("type");
 const algorithmSelector = document.getElementById("algorithm");
 const visualizationDiv = document.getElementById("visualization");
 const timeSpan = document.getElementById("time-counter");
+const algorithmsForm = document.getElementById("algorithms-form");
 
 const schedulingAlgorithms = [
   { value: "fcfs", text: "First Come First Serve" },
@@ -11,7 +12,7 @@ const schedulingAlgorithms = [
   { value: "sjf", text: "Shortest Job First" },
   { value: "srt", text: "Shortest Remaining Time" },
   { value: "hrrn", text: "Highest Response-Ratio Next" },
-  { value: "mfq", text: "Multilevel Feedback Queues" },
+  { value: "mfq", text: "Multilevel Feedback Queues" }
 ];
 
 const pageReplacementAlgorithms = [
@@ -19,9 +20,9 @@ const pageReplacementAlgorithms = [
   { value: "lru", text: "Least Recently Used" },
   { value: "lfu", text: "Least Frequently Used" },
   { value: "nru", text: "Not Recently Used" },
-  { value: "rpo", text: "Reemplazo de Página Optimal" },
+  { value: "rpo", text: "Reemplazo de Página Optimal" }
 ];
-  
+
 // Show different algorithms depending on the selector type 
 const setAlgorithms = () => {
   // Remove all options
@@ -39,6 +40,7 @@ const setAlgorithms = () => {
     algorithmSelector.add(option);
   });
 
+  // Select first option as default
   algorithmSelector.value = "fcfs";
 };
 
@@ -54,18 +56,36 @@ const clearLogs = () => {
 
 // Read time units from a file
 const readTime = event => {
+  // Get file
   const input = event.target;
   const file = input.files[0];
+
+  // Get content
   const reader = new FileReader();
   reader.readAsText(file);
   reader.onload = () => {
+    // Set values
     const content = reader.result;
     timeSpan.textContent = parseInt(content);
     currentTime = parseInt(content);
   }
 }
 
-/* Algorithms */
+// Execute the selected algorithm
+const run = event => {
+  // Prevent page reloading
+  event.preventDefault();
+
+  // Run the function
+  const selectedFunction = algorithms[typeSelector.value][algorithmSelector.value];
+  selectedFunction();
+}
+
+/**
+  *
+  * ALGORITHMS 
+  * 
+  */
 
 class Process {
   constructor(id, arrival, burst, status) {
@@ -76,75 +96,110 @@ class Process {
   }
 }
 
-// List of all processes
-let processes = [
-  new Process(0, 0, 3, 0),
-  new Process(1, 2, 5, 0),
-  new Process(2, 4, 4, 0),
-  new Process(3, 6, 1, 0),
-  new Process(4, 8, 2, 0)
-];
-
-// Queue, elements who have already arrived
-let queue = [];
-
+// Global variables
 let currentTime = 0;
-let numProcesses = processes.length;
-let done = 0;
 
-while (done != numProcesses) {
-   
-  // See if new processes have arrived
-  // Add them to queue if so
-  for (let i = 0; i < numProcesses; i++) {
-    if (processes[i].arrival <= currentTime && processes[i].status == 0) {
-      queue.push(processes[i]);
-      processes[i].status = 1;
-      displayLog("Llego proceso: " + processes[i].id);
-      displayLog("Tiempo de llegada: " + processes[i].arrival);
-    }
-  }
+// First Come First Serve - Scheduling Algorithm
+const fcfsScheduling = () => {
+  // List of all processes
+  let processes = [
+    new Process(0, 0, 3, 0),
+    new Process(1, 2, 5, 0),
+    new Process(2, 4, 4, 0),
+    new Process(3, 6, 1, 0),
+    new Process(4, 8, 2, 0)
+  ];
 
-  // Check if there are any processes on the queue
-  // If not, continue
-  if (queue.length == 0) {
-    displayLog("Cola vacía");
-    currentTime += 1;
-    continue;
-  }
+  // Queue, elements who have already arrived
+  let queue = [];
+  let numProcesses = processes.length;
+  let done = 0;
 
-  // If there is only one process, execute it
-  if (queue.length == 1) {
-    displayLog("-- Se ejecuto el proceso: " + queue[0].id + " --");
-    currentTime += queue[0].burst;
-    queue.pop();
-    done += 1;
-    continue;
-  }
+  while (done != numProcesses) {
 
-  // If there are more than one processes queued up, find next
-  if (queue.length > 1) {
-      
-    // Find next process
-    let nextP = queue[0];
-    let maxRR = ((currentTime-queue[0].arrival)+queue[0].burst)/queue[0].burst;
-
-    for (let i = 1; i < queue.length; i++) {
-      let RR = ((currentTime-queue[i].arrival)+queue[i].burst)/queue[i].burst;
-      if (RR > maxRR) {
-        maxRR = RR;
-        nextP = queue[i];
+    // See if new processes have arrived
+    // Add them to queue if so
+    for (let i = 0; i < numProcesses; i++) {
+      if (processes[i].arrival <= currentTime && processes[i].status == 0) {
+        queue.push(processes[i]);
+        processes[i].status = 1;
+        displayLog("Llego proceso: " + processes[i].id);
+        displayLog("Tiempo de llegada: " + processes[i].arrival);
       }
     }
-    displayLog("Siguiente proceso: " + nextP.id + " con RR " + maxRR);
-    displayLog("Tiempo actual: " + currentTime);
+    // Check if there are any processes on the queue
+    // If not, continue
+    if (queue.length == 0) {
+      displayLog("Cola vacía");
+      currentTime += 1;
+      continue;
+    }
 
-    // Execute process
-    currentTime += nextP.burst;
-    done += 1;
-      
-    // Remove process from queue
-    let index = queue.indexOf(nextP);
-    queue.splice(index, 1);
+    // If there is only one process, execute it
+    if (queue.length == 1) {
+      displayLog("-- Se ejecuto el proceso: " + queue[0].id + " --");
+      currentTime += queue[0].burst;
+      queue.pop();
+      done += 1;
+      continue;
+    }
+
+    // If there are more than one processes queued up, find next
+    if (queue.length > 1) {
+
+      // Find next process
+      let nextP = queue[0];
+      let maxRR = ((currentTime-queue[0].arrival)+queue[0].burst)/queue[0].burst;
+
+      for (let i = 1; i < queue.length; i++) {
+        let RR = ((currentTime-queue[i].arrival)+queue[i].burst)/queue[i].burst;
+        if (RR > maxRR) {
+          maxRR = RR;
+          nextP = queue[i];
+        }
+      }
+      displayLog("Siguiente proceso: " + nextP.id + " con RR " + maxRR);
+      displayLog("Tiempo actual: " + currentTime);
+
+      // Execute process
+      currentTime += nextP.burst;
+      done += 1;
+
+      // Remove process from queue
+      let index = queue.indexOf(nextP);
+      queue.splice(index, 1);
+    }
+  }
+}
+
+const fifoScheduling = () => console.log('1');
+const rrScheduling = () => console.log('2');
+const sjfScheduling = () => console.log('3');
+const srtScheduling = () => console.log('4');
+const hrrnScheduling = () => console.log('5');
+const mfqScheduling = () => console.log('6');
+const fcfsPageReplacement = () => console.log('7');
+const lruPageReplacement = () => console.log('8');
+const lfuPageReplacement = () => console.log('9');
+const nruPageReplacement = () => console.log('10');
+const rpoPageReplacement = () => console.log('11');
+
+// Object containing all algorithms
+const algorithms = {
+  scheduling: {
+    fcfs: fcfsScheduling,
+    fifo: fifoScheduling,
+    rr: rrScheduling,
+    sjf: sjfScheduling,
+    srt: srtScheduling,
+    hrrn: hrrnScheduling,
+    mfq: mfqScheduling
+  },
+  pageReplacement: {
+    fcfs: fcfsPageReplacement,
+    lru: lruPageReplacement,
+    lfu: lfuPageReplacement,
+    nru: nruPageReplacement,
+    rpo: rpoPageReplacement
   }
 }
